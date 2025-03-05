27cafff519a8d73453a77a8c71ea4f01fff0ac63
M Rakib Ahmed <ahmedmrakib24@gmail.com>
2025-03-05 20:05:34 +0600

    new file added

--- src/components/Avalibale.jsx
+++ src/components/Avalibale.jsx
@@ -23,7 +23,7 @@ const Avalibale = () => {
                             </figure>
                             <div className="">
                                 <h2 className="card-title">Card Title</h2>
-                                <p>A card component has a figure, a body part, and inside body there are title and actions parts</p>
+                                <p>A card component hasa a figure, a body part, and inside body there are title and actions parts</p>
                                 <div className="card-actions justify-end">
                                     <button className="btn btn-primary">Buy Now</button>
                                 </div>
